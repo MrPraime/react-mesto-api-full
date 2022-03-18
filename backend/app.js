3dcecb0db34f3b558152a3bc76d662a785251a43
MrPraime <g--praime@yandex.ru>
2022-03-18 12:49:15 +0300

    15 rabot!

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/errorHandling');
@@ -13,6 +14,9 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors({
+  Origin: 'https://praime.nomoredomains.work',
+}));
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', () => {
